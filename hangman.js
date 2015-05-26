81e21075bc80487d695bc008082ebc3391d08968
Thomas McKee <caffeine.storm@gmail.com>
2015-05-26 23:32:18 +0000

    Including updates we worked on in class

--- hangman.js
+++ hangman.js
@@ -1,13 +1,35 @@
 var word = "hello";
+var boardState = ["_", "_", "_", "_", "_"];
+
+function updateBoard() {
+    var board = document.getElementById("secretWord");
+    board.innerHTML = boardState[0] + ' '
+    + boardState[1] + ' '
+    + boardState[2] + ' '
+    + boardState[3] + ' '
+    + boardState[4];
+}
 
 function handleLoad() {
-    var board = document.getElementById("secretWord");
-    board.innerHTML = "_ _ _ _ _";
+    updateBoard();
 }
 
 function handleKeyPress(event) {
-    var key = event.key;
-    console.log(String.fromCharCode(event.keyCode));
+    var key = String.fromCharCode(event.keyCode);
+
+    var character = word[0];
+    console.log("key: " + key, "character: " + character);
+    if(key === character) {
+        boardState[0] = character;
+    }
+    if(boardState[0] == word[0]) {
+
+        var character1 = word[1];
+        if(key == character1) {
+            boardState[1] = character1;
+        }
+    }
+    updateBoard();
 }
 
 document.addEventListener("DOMContentLoaded", handleLoad, false);
