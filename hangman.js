a87c1ad2b103105e0b5a3f04d1250c010706fcd5
Thomas McKee <caffeine.storm@gmail.com>
2015-06-10 19:55:55 +0000

    Updates from class

--- hangman.js
+++ hangman.js
@@ -1,6 +1,10 @@
 var word = "hello";
+var guessesLeft = 10;
+
+// This tracks what the user sees. To update the page, call 'updateBoard'.
 var boardState = ["_", "_", "_", "_", "_"];
 
+// This function draws the boardState to the screen.
 function updateBoard() {
     var board = document.getElementById("secretWord");
     board.innerHTML = boardState[0] + ' '
@@ -14,21 +18,60 @@ function handleLoad() {
     updateBoard();
 }
 
+
+function printGuessesLeft() {
+    var message = document.getElementById("guessesLeft");
+    console.log("printGuessesLeft: guessesLeft: " + guessesLeft);
+    message.innerHTML = "" + guessesLeft;
+}
+
+function game_over() {
+    console.log("GAME OVER!");
+}
+
 function handleKeyPress(event) {
     var key = String.fromCharCode(event.keyCode);
+    var shouldPenalize = true;
 
-    var character = word[0];
-    console.log("key: " + key, "character: " + character);
-    if(key === character) {
-        boardState[0] = character;
-    }
-    if(boardState[0] == word[0]) {
+    console.log("key: " + key);
 
-        var character1 = word[1];
-        if(key == character1) {
-            boardState[1] = character1;
+    // for( initializer ; condition ; after-thought ) {
+    //    body;
+    // }
+    
+    // 1) Run the initializer
+    // 2) Check the condition
+    // 3) a) If the condition is true, run the body, then head to step 4
+    //    b) If the condition is false, jump past closing brace, stop looping
+    // 4) after-thought
+    // 5) go to 2)
+
+    console.log("Before for loop, guessesLeft: " + guessesLeft);
+
+    // Each time a key is pressed
+    for( var i = 0; i < word.length; i++ ) {
+    //  Check if the i'th character matches
+        if( word[i] === key ) {
+    //      If so, mark the character
+            boardState[i] = word[i];
+            shouldPenalize = false;
         }
     }
+
+    if(shouldPenalize) {
+        // Every time the user's guess was wrong
+        //    Penalize the user
+        guessesLeft--;
+        // Update the page with how many guesses are left
+        printGuessesLeft();
+        //    If the user runs out of guesses, game over!
+        if(guessesLeft <= 0) {
+            game_over();
+        }
+    }
+
+    console.log("After for loop, guessesLeft: ", guessesLeft);
+
     updateBoard();
 }
 
